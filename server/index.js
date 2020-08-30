566346d17a509b3668ab2c740e0ac813d371b1a3
Jean-Marc M <jeanmarcmoeckel@gmail.com>
2020-08-30 20:01:51 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const presidentsList = require("../data/presidents.json");
+const isEmptyObj = require("./utils/isEmptyObj");
+
+const app = express();
+
+app.get("/presidents", (req, res) => {
+  const noQuery = isEmptyObj(req.query);
+
+  // NO QUERY
+  if (noQuery) {
+    return res.send({
+      error: "Please provide a valid query",
+    });
+  }
+
+  // RESULT QUERIES
+  if ("result" in req.query) {
+    if (req.query.result === "all") {
+      // 1) Filter presidents by party
+      if ("party" in req.query && req.query.party) {
+        filteredList = presidentsList.filter((president) => {
+          return president.party.toLowerCase() === req.query.party.toLowerCase();
+        });
+        return res.send({
+          presidents: filteredList,
+        });
+      }
+      // 2) Filter presidents by election year range
+      if ("election" in req.query && req.query.election) {
+        const election = req.query.election.split(",");
+        const start = parseInt(election[0]);
+        const end = parseInt(election[1]);
+
+        // Filter all whose election was in between the given range
+        filteredList = presidentsList.filter((president) => {
+          const elected = president.election[0] || president.elected[1];
+          return elected >= start && elected <= end;
+        });
+        return res.send({
+          presidents: filteredList,
+        });
+      }
+      // 3) Send all presidents
+      return res.send({
+        presidents: presidentsList,
+      });
+    }
+    // 4) Send a random president
+    if (req.query.result === "random") {
+      const randomIndex = Math.floor(Math.random() * (presidentsList.length - 1));
+      return res.send({
+        president: presidentsList[randomIndex],
+      });
+    }
+  }
+
+  // SEARCH QUERIES
+  if ("search" in req.query) {
+    const searchName = req.query.search;
+    // Search for all presidents who has got the searchName inside their name property
+    const searchResults = presidentsList.filter((president) => {
+      return president.name.toLowerCase().includes(searchName);
+    });
+    return res.send({
+      presidents: searchResults,
+    });
+  }
+
+  // NO DATA FOUND
+  res.send({
+    error: "Couldn't find any data",
+  });
+});
+
+app.listen("3000", () => console.log("Server up and running on port 3000"));
---
+++ server/utils/isEmptyObj.js
@@ -0,0 +1,10 @@
+const isEmptyObj = (obj) => {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = isEmptyObj;
